f29c1b2c3a39f7a8908a910e905a4b404cbe4d90
Emre Yılmaz <hintlikaraemre@gmail.com>
2020-11-13 16:03:23 +0300

    navbar and content design

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,123 @@
+import React from "react"
+import styles from './Navbar.module.css'
+
+
+const Navbar = () => {
+    return (
+        <div className={styles.navbar}>
+            
+           <h2 className={styles.toptitle}>Gündem</h2>
+
+            <div className={styles.title}>
+                <p className={styles.titlecontent}>
+                    Efdal İNCESU işe girdi
+                </p>
+                <p className={styles.commentcount}>
+                    124
+                </p>
+            </div>
+
+            <div className={styles.title}>
+                <p className={styles.titlecontent}>
+                    Priviada son olan olaylar. Privia ibne mi
+                </p>
+                <p className={styles.commentcount}>
+                    24
+                </p>
+            </div>
+
+            <div className={styles.title}>
+                <p className={styles.titlecontent}>
+                    Efdal Hypea kapağı attı şimdi neler olacak
+                </p>
+                <p className={styles.commentcount}>
+                    24
+                </p>
+            </div>
+
+
+            <div className={styles.title}>
+                <p className={styles.titlecontent}>
+                    HAyırlı olsun 
+                </p>
+                <p className={styles.commentcount}>
+                    24
+                </p>
+            </div>
+
+            <div className={styles.title}>
+                <p className={styles.titlecontent}>
+                    Türkiyede iyi şeylerde oluyor. Her şey güzel olacak
+                </p>
+                <p className={styles.commentcount}>
+                    24
+                </p>
+            </div>
+
+
+            <div className={styles.title}>
+                <p className={styles.titlecontent}>
+                    Efdalın işe girmesi hakkında ne düşünüyorsunuz
+                </p>
+                <p className={styles.commentcount}>
+                    24
+                </p>
+            </div>
+
+            <div className={styles.title}>
+                <p className={styles.titlecontent}>
+                    Sivas elllerinde bulamadım türküsü ne zaman açıldı
+                </p>
+                <p className={styles.commentcount}>
+                    24
+                </p>
+            </div>
+
+            <div className={styles.title}>
+                <p className={styles.titlecontent}>
+                    Sivas elllerinde bulamadım türküsü ne zaman açıldı
+                </p>
+                <p className={styles.commentcount}>
+                    24
+                </p>
+            </div>
+
+            <div className={styles.title}>
+                <p className={styles.titlecontent}>
+                    Sivas elllerinde bulamadım türküsü ne zaman açıldı
+                </p>
+                <p className={styles.commentcount}>
+                    24
+                </p>
+            </div>
+
+
+            <div className={styles.title}>
+                <p className={styles.titlecontent}>
+                    Sivas elllerinde bulamadım türküsü ne zaman açıldı
+                </p>
+                <p className={styles.commentcount}>
+                    24
+                </p>
+            </div>
+
+
+            <div className={styles.title}>
+                <p className={styles.titlecontent}>
+                    Sivas elllerinde bulamadım türküsü ne zaman açıldı
+                </p>
+                <p className={styles.commentcount}>
+                    24
+                </p>
+            </div>
+            
+            
+            
+
+        </div>
+    )
+}
+
+export default Navbar
+
+
