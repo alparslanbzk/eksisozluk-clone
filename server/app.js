23912fc75ad43238680a3296732410d6f2d743bc
Emre Yılmaz <hintlikaraemre@gmail.com>
2020-11-14 19:52:19 +0300

    mongoose is running

--- server/app.js
+++ server/app.js
@@ -1,7 +1,27 @@
 const express = require("express")
 const app = express()
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+dotenv.config()
 
 const PORT = 5000
+
+mongoose.connect(process.env.MONGO_URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+})
+
+mongoose.connection.on("connected",() => {
+    console.log("mongoose is running")
+})
+
+mongoose.connection.on("error",(err) => {
+    console.log("mongoose is crushed", err)
+})
+
+
 
 app.get("/",(req,res) => {
     res.send("Home Page")
