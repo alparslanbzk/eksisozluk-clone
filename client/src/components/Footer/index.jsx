ae9159075145b461ed5e71e6f20bd5776f2b88f6
Emre Yılmaz <hintlikaraemre@gmail.com>
2020-11-14 10:51:43 +0300

    footer and banner

---
+++ client/src/components/Footer/index.jsx
@@ -0,0 +1,33 @@
+import React from "react"
+import styles from "./Footer.module.css"
+
+const Footer = () => {
+    return (
+        <div className={styles.footer}>
+            <div className={styles.list}>
+                    <a href="/#">iletişim</a>
+                    <a href="/#">reklam</a>
+                    <a href="/#">kariyer</a>
+                    <a href="/#">kullanım koşulları</a>
+                    <a href="/#">gizlilik politikamız</a>
+                    <a href="/#">sss</a>
+                    <a href="/#">istatistikler</a>
+                    <a href="/#">sub-etha</a>
+                    <a href="/#">instagram</a>
+                    <a href="/#">twitter</a>
+                    <a href="/#">facebook</a>
+
+
+                    
+
+            </div>
+        </div>
+    )
+}
+
+
+
+export default Footer
+
+
+
