9a001fe3d965f4e533902c8621082f3c6f328011
Emre Yılmaz <hintlikaraemre@gmail.com>
2020-11-14 10:33:40 +0300

    entry completed

--- client/src/components/Entry/index.jsx
+++ client/src/components/Entry/index.jsx
@@ -5,6 +5,9 @@ import styles from "./Entry.module.css"
 const Entry = () => {
     return (
         <div className={styles.entry}>
+            
+
+
 
             <div>
             <a className={styles.entrytitle} href="#">sağlık bakanlığı'nın ankara suyu raporu</a>
@@ -16,7 +19,10 @@ melih? yaşıyor musun? </p>
 
                 <div className={styles.bottomMenu}>
                 <div className={styles.icons}>
-
+                <img src="/facebook.svg" alt="facebook" />
+                <img src="/twitter.svg" alt="facebook" />
+                <img className={styles.rotatearrow} src="/arrow.svg" alt="facebook" />
+                <img  src="/arrow.svg" alt="facebook" />
                 </div>
                 <div className={styles.nick}>
                     <a className={styles.date}>06.01.2018 13:05 ~ 13:53</a>
@@ -36,7 +42,10 @@ melih? yaşıyor musun? </p>
 
                 <div className={styles.bottomMenu}>
                 <div className={styles.icons}>
-
+                <img src="/facebook.svg" alt="facebook" />
+                <img src="/twitter.svg" alt="facebook" />
+                <img className={styles.rotatearrow} src="/arrow.svg" alt="facebook" />
+                <img  src="/arrow.svg" alt="facebook" />
                 </div>
                 <div className={styles.nick}>
                     <a className={styles.date}>06.01.2018 13:05 ~ 13:53</a>
@@ -56,7 +65,10 @@ melih? yaşıyor musun? </p>
 
                 <div className={styles.bottomMenu}>
                 <div className={styles.icons}>
-
+                <img src="/facebook.svg" alt="facebook" />
+                <img src="/twitter.svg" alt="facebook" />
+                <img className={styles.rotatearrow} src="/arrow.svg" alt="facebook" />
+                <img  src="/arrow.svg" alt="facebook" />
                 </div>
                 <div className={styles.nick}>
                     <a className={styles.date}>06.01.2018 13:05 ~ 13:53</a>
@@ -76,7 +88,10 @@ melih? yaşıyor musun? </p>
 
                 <div className={styles.bottomMenu}>
                 <div className={styles.icons}>
-
+                <img src="/facebook.svg" alt="facebook" />
+                <img src="/twitter.svg" alt="facebook" />
+                <img className={styles.rotatearrow} src="/arrow.svg" alt="facebook" />
+                <img  src="/arrow.svg" alt="facebook" />
                 </div>
                 <div className={styles.nick}>
                     <a className={styles.date}>06.01.2018 13:05 ~ 13:53</a>
@@ -84,6 +99,32 @@ melih? yaşıyor musun? </p>
                 </div>
                 </div>
             </div>
+
+
+            <div>
+            <a className={styles.entrytitle} href="#">sağlık bakanlığı'nın ankara suyu raporu</a>
+            <p className={styles.entrybody}>diyor ki;
+            "demir, nitrat, aluminyum, arsenik, perfringens, enterokok, eschericis coli ve koliform bakteri parametreleri yönünden insani tüketim amaçlı sular hakkında yönetmelikte belirtilen sınır değerlere göre uygunsuz ve serbest klor düzeyinin yetersiz olduğu saptanmıştır."
+            görüldüğü üzere içinde bir biz yokuz.
+melih? yaşıyor musun? </p>
+
+
+                <div className={styles.bottomMenu}>
+                <div className={styles.icons}>
+                <img src="/facebook.svg" alt="facebook" />
+                <img src="/twitter.svg" alt="facebook" />
+                <img className={styles.rotatearrow} src="/arrow.svg" alt="facebook" />
+                <img  src="/arrow.svg" alt="facebook" />
+                </div>
+                <div className={styles.nick}>
+                    <a className={styles.date}>06.01.2018 13:05 ~ 13:53</a>
+                    <a className={styles.name}> sanikmagdur</a>
+                </div>
+                </div>
+            </div>
+
+
+           
             
 
             
