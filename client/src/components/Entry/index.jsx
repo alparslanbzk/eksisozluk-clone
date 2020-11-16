507fb212786231c08d6170f68f61c74ede84a976
Emre Yılmaz <hintlikaraemre@gmail.com>
2020-11-16 14:34:06 +0300

    like unlike refactoring client

--- client/src/components/Entry/index.jsx
+++ client/src/components/Entry/index.jsx
@@ -1,96 +1,121 @@
-import React , {useState,useEffect} from "react"
+import React, { useState, useEffect } from "react"
 import styles from "./Entry.module.css"
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 import axios from "axios"
 
 const Entry = () => {
 
-    const [data,setData] = useState([])
+    const [data, setData] = useState([])
+    const [islike, setIsLike] = useState(false)
 
     useEffect(() => {
         axios.get('/allpost')
-        .then(response => {
-            console.log(response.data.posts)
-            setData(response.data.posts)
-        })
-    },[] )
+            .then(response => {
+                // console.log(response.data.posts)
+                setData(response.data.posts)
+            })
+    }, [data])
 
 
-    const likePost = (id) => {
-
-        axios.put('/like',{
-            postId:id
-        },{
-            headers:  {
-                "Authorization":"Bearer "+localStorage.getItem("jwt")
-
-            }
-        }).then(response=> {
-            // if(response.error){
-            //     console.log(response.error)
-            // }
-            console.log(response)
-        })
+    const likePost = async (id) => {
         
+        if (islike) {
+            return null
+        } else {
+            axios.put('/like', {
+                postId: id
+            }, {
+                headers: {
+                    "Authorization": "Bearer " + localStorage.getItem("jwt")
+
+                }
+            }).then(response => {
+                
+            const newData = data.map(item=>{
+                if(item._id == response._id){
+                    return response
+                }else{
+                    return item
+                }
+            })
+
+            setData(newData) 
+                
+            })
+        }
+
+
+
     }
 
 
 
     const unlikePost = (id) => {
-        axios.put('/unlike',{
-            postId:id
-        },{
-            headers:{
-                "Authorization":"Bearer "+localStorage.getItem("jwt")
+        axios.put('/unlike', {
+            postId: id
+        }, {
+            headers: {
+                "Authorization": "Bearer " + localStorage.getItem("jwt")
             }
-        }).then(result => {
-            console.log(result)
+        }).then(response => {
+            
+            const newData = data.map(item=>{
+                if(item._id == response._id){
+                    return response
+                }else{
+                    return item
+                }
+            })
+
+            
+            setData(newData)
         })
     }
 
     return (
         <div className={styles.entry}>
-            
+
 
             {data.map(items => {
-                return  (
+                return (
                     <div key={items._id}>
-            <Link to="/" className={styles.entrytitle} href="#">{items.title}</Link>
-            <p className={styles.entrybody}>{items.body}</p>
+                        <Link to="/" className={styles.entrytitle} href="#">{items.title}</Link>
+                        <p className={styles.entrybody}>{items.body}</p>
 
 
-                <div className={styles.bottomMenu}>
-                <div className={styles.icons}>
-                <img src="/facebook.svg" alt="facebook" />
-                <img src="/twitter.svg" alt="facebook" />
-                <img className={styles.rotatearrow} src="/arrow.svg" alt="facebook" 
-                onClick={() => {
-                    likePost(items._id)
-                }}
-                />
-                <img  src="/arrow.svg" alt="facebook" onClick={() => {
-                    unlikePost(items._id)
-                }}/>
-                </div>
-                <div className={styles.nick}>
-                    <Link to="/"  className={styles.date}>06.01.2018 13:05 ~ 13:53</Link>
-                    <Link  to="/profile"  className={styles.name}> {items.postedBy.nick}</Link>
-                </div>
-                </div>
-            </div>
+                        <div className={styles.bottomMenu}>
+                            <div className={styles.icons}>
+                                <img src="/facebook.svg" alt="facebook" />
+                                <img src="/twitter.svg" alt="facebook" />
+                                <img className={styles.rotatearrow} src="/arrow.svg" alt="facebook"
+                                    onClick={() => {
+                                        likePost(items._id)
+                                    }}
+                                />
+                                <img src="/arrow.svg" alt="facebook" onClick={() => {
+                                    unlikePost(items._id)
+                                }} />
+                                <small className={styles.likeslength}>{items.likes.length}</small>
+                            </div>
+                            <div className={styles.nick}>
+                                <Link to="/" className={styles.date}>06.01.2018 13:05 ~ 13:53</Link>
+                                <Link to="/profile" className={styles.name}> {items.postedBy.nick}</Link>
+                            </div>
+                        </div>
+                    </div>
                 )
             })}
-            
 
 
 
-            
-            
 
-          
-            
 
-            
+
+
+
+
+
+
 
         </div>
 
